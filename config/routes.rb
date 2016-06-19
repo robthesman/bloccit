10e8a7aac2d554e9f315dede82f3d287cafbbb2a
robthesman <robthesman@gmail.com>
2016-06-19 22:57:23 +0000

    Assignment 32 CRUD incomplete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  
+  resources :advertisements
   resources :posts
 
   get 'about' => 'welcome#about'
 
-  root 'welcome#index'
+  root to: 'welcome#index'
   
   
 end
