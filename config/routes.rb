c59363a06fd7a37bdd4b41a03d9b763c8279c918
robthesman <robthesman@gmail.com>
2016-06-22 16:47:35 +0000

    Assignment 33 more crud: completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+
   resources :posts
+  resources :questions
 
   get 'about' => 'welcome#about'
 
-  root 'welcome#index'
+  root to: 'welcome#index'
   
   
 end
