99a30a069709f13974ee39a4c029030a6226eec2
robthesman <robthesman@gmail.com>
2016-07-05 20:06:56 +0000

    Assignment 37 authentication: completed

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
      resources :posts, except: [:index]
    end
    
-   resources :users, only: [:new, :create]
-   
+  resources :users, only: [:new, :create]
+
+  post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
+  
+ 
   
   
 end
