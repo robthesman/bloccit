692b68bceca68c1aace5545cd83d75dc3e21d205
robthesman <robthesman@gmail.com>
2016-07-10 18:33:16 +0000

    Checkpoint 39 posts & users: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,17 @@
 require 'random_data'
 
+ # Create Users
+ 5.times do
+   User.create!(
+ # #3
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
+
+
  # Create Topics
  15.times do
    Topic.create!(
@@ -13,6 +25,7 @@ require 'random_data'
  # Create Posts
  50.times do
    Post.create!(
+     user:   users.sample,
      topic:  topics.sample, 
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -30,7 +43,14 @@ require 'random_data'
    )
  end
  
+  user = User.first
+  user.update_attributes!(
+   email: 'robthesman@gmail.com', # replace this with your personal email
+   password: 'helloworld'
+ )
+ 
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
