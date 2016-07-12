230ecbb241a7954335d012bb81940f498358702d
robthesman <robthesman@gmail.com>
2016-07-12 01:47:45 +0000

    Checkpoint 40 authorization: completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
    has_many :posts
 
    before_save { self.email = email.downcase if email.present? }
+   before_save { self.role ||= :member }
 
    validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
@@ -16,5 +17,7 @@ class User < ActiveRecord::Base
 
 
    has_secure_password
+   
+   enum role: [:member, :admin]
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -43,9 +43,18 @@ require 'random_data'
    )
  end
  
-  user = User.first
-  user.update_attributes!(
-   email: 'robthesman@gmail.com', # replace this with your personal email
+ # Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
  
