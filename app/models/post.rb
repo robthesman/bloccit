684d4990b24f641c5ef612c2baaca254cc70d925
robthesman <robthesman@gmail.com>
2016-07-25 18:52:19 +0000

    Assignment 43 voting: completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,12 +8,14 @@ class Post < ActiveRecord::Base
     has_many :labelings, as: :labelable
     has_many :labels, through: :labelings
 
-   default_scope { order('rank DESC') }
+    after_create :create_vote
+
+    default_scope { order('rank DESC') }
     
-   validates :title, length: { minimum: 5 }, presence: true
-   validates :body, length: { minimum: 20 }, presence: true
-   validates :topic, presence: true
-   validates :user, presence: true
+    validates :title, length: { minimum: 5 }, presence: true
+    validates :body, length: { minimum: 20 }, presence: true
+    validates :topic, presence: true
+    validates :user, presence: true
 
    def up_votes
      votes.where(value: 1).count
@@ -32,6 +34,12 @@ class Post < ActiveRecord::Base
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
    end    
+   
+   private
+   
+   def create_vote
+       user.votes.create(value: 1, post: self)
+   end
     
 end
 
