02158801063c66067dfb2578ccada6e2f2296142
robthesman <robthesman@gmail.com>
2016-07-31 22:31:51 +0000

    Checkploint 46 Private Topics: completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,13 +1,12 @@
 class Topic < ActiveRecord::Base
-    has_many :posts, dependent: :destroy
-
-    has_many :labelings, as: :labelable
     
+    has_many :posts, dependent: :destroy
+    has_many :labelings, as: :labelable
     has_many :labels, through: :labelings
     
     validates :name, length: { minimum: 5 }, presence: true
     validates :description, length: { minimum: 15 }, presence: true
     
-
+    scope :visible_to, -> (user) { user ? all : where(public: true) }
     
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -15,16 +15,31 @@ RSpec.describe Topic, type: :model do
     it { is_expected.to have_many(:labelings) }
     it { is_expected.to have_many(:labels) }
     
-
- # #1
    describe "attributes" do
      it "has name and description attributes" do
        expect(topic).to have_attributes(name: topic.name, description: topic.description)
      end
 
- # #2
      it "is public by default" do
        expect(topic.public).to be(true)
+     end
+   end
+   
+   describe "scopes" do
+     before do
+       @public_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)
+       @private_topic = Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph, public: false)
+     end
+ 
+     describe "visible_to(user)" do
+       it "returns all topics if the user is present" do
+         user = User.new
+         expect(Topic.visible_to(user)).to eq(Topic.all)
+       end
+ 
+       it "returns only public topics if user is nil" do
+         expect(Topic.visible_to(nil)).to eq([@public_topic])
+       end
      end
    end
   
