ff25fd9e92e0d012e80e61336be7aa71adc2222f
robthesman <robthesman@gmail.com>
2016-08-01 22:30:16 +0000

    Checkpoint 47 ajax: completed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,49 +1,55 @@
 class CommentsController < ApplicationController
-    
-   # #10
+
    before_action :require_sign_in
    
    before_action :authorize_user, only: [:destroy]  
  
    def create
- # #11
+
      @post = Post.find(params[:post_id])
-     comment = @post.comments.new(comment_params)
-     comment.user = current_user
+     @comment = @post.comments.new(comment_params)
+     @comment.user = current_user
+     @new_comment = Comment.new 
  
-     if comment.save
+     if @comment.save
        flash[:notice] = "Comment saved successfully."
- # #12
-       redirect_to [@post.topic, @post]
      else
        flash[:alert] = "Comment failed to save."
- # #13
-       redirect_to [@post.topic, @post]
      end
+     
+      respond_to do |format|
+       format.html
+       format.js
+     end    
+     
    end
  
     def destroy
      @post = Post.find(params[:post_id])
-     comment = @post.comments.find(params[:id])
+     @comment = @post.comments.find(params[:id])
  
-     if comment.destroy
+     if @comment.destroy
        flash[:notice] = "Comment was deleted successfully."
-       redirect_to [@post.topic, @post]
+
      else
        flash[:alert] = "Comment couldn't be deleted. Try again."
-       redirect_to [@post.topic, @post]
+
      end
+     
+     respond_to do |format|
+      format.html
+      format.js
+     end
+     
     end
  
    private
  
- # #14
+
    def comment_params
      params.require(:comment).permit(:body)
    end  
-   
- 
- # #16
+
    def authorize_user
      comment = Comment.find(params[:id])
      unless current_user == comment.user || current_user.admin?
