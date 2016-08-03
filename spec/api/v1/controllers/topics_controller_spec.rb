8f32be52a36236648ddd00cfb77cbcc54f1f85c9
robthesman <robthesman@gmail.com>
2016-08-03 20:16:30 +0000

    Assignment 48 Ajax: completed

--- spec/api/v1/controllers/topics_controller_spec.rb
+++ spec/api/v1/controllers/topics_controller_spec.rb
@@ -3,6 +3,7 @@
  RSpec.describe Api::V1::TopicsController, type: :controller do
    let(:my_user) { create(:user) }
    let(:my_topic) { create(:topic) }
+   let(:my_post) { create(:post, topic: my_topic, user: my_user) }
 
    context "unauthenticated user" do
      it "GET index returns http success" do
@@ -14,6 +15,13 @@
        get :show, id: my_topic.id
        expect(response).to have_http_status(:success)
      end
+     
+     it "GET show returns child posts" do 
+         get :show, id: my_topic.id
+         response_hash = JSON.parse response.body
+         expect(response_hash['posts']).to_not be_nil
+     end
+     
    end
  
    context "unauthorized user" do
@@ -30,5 +38,12 @@
        get :show, id: my_topic.id
        expect(response).to have_http_status(:success)
      end
+     
+     it "GET show returns child posts" do 
+         get :show, id: my_topic.id
+         response_hash = JSON.parse response.body
+         expect(response_hash['posts']).to_not be_nil
+     end
+     
    end
  end
